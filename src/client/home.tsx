90b2d60ea3d1615dc96f05e92c23f7596beb6d73
中島浩二 <knaka0209@gmail.com>
2024-09-26 18:48:08 +0900

    up, 0926a5

---
+++ src/client/home.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+console.log("#about.tsx");
+import Head from '../components/Head'
+//
+function Page() {
+    return (
+    <div className="container mx-auto my-2 px-8 bg-white">
+        <Head />
+        <h1 className="text-4xl text-gray-700 font-bold my-2"
+        >home!</h1>
+    </div>
+    )
+}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <Page />
+);
+
+  
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import fs from 'node:fs/promises'
+import express from "express";
+import { renderToString } from 'react-dom/server';
+
+import Top from './pages/App';
+import About from './pages/about';
+//
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+console.log("env= ", process.env.NODE_ENV);
+//
+const errorObj = {ret: "NG", messase: "Error"};
+//MPA 
+app.get('/about', (req: any, res: any) => {
+  try {
+    res.send(renderToString(About()));
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+app.get("/*", (req, res) => {
+  res.send(renderToString(Top()));
+});
+//start
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
+  
